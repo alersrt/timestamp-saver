3fe14b46464d8449d951f93a573b273f17120a0c
alersrt <strtmg@yandex.ru>
2018-10-05 05:12:05 +0700

    Extract threads' bodies to external static methods and add processing of the param

--- src/main/java/org/student/Application.java
+++ src/main/java/org/student/Application.java
@@ -35,63 +35,88 @@ public class Application {
    * @param args array of arguments.
    */
   public static void main(String[] args) {
-    var timerThread = new Thread(() -> {
-      while (true) {
-        try {
-          TimeUnit.SECONDS.sleep(1);
-          queue.offer(LocalDateTime.now());
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    }, "#timer");
+    if (args.length == 0) {
+      var timerThread = new Thread(Application::runTimer, "#timer");
+      var databaseThread = new Thread(Application::runSaver, "#database");
+      timerThread.start();
+      databaseThread.start();
+    } else if (args.length == 1 && args[0].equals("-p")) {
+      var printerThread = new Thread(Application::runLister, "#list");
+      printerThread.start();
+    }
+  }
 
-    var databaseThread = new Thread(() -> {
+  private static void runTimer() {
+    while (true) {
       try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-      } catch (ClassNotFoundException e) {
+        TimeUnit.SECONDS.sleep(1);
+        queue.offer(LocalDateTime.now());
+      } catch (Exception e) {
         e.printStackTrace();
       }
+    }
+  }
 
-      while (true) {
-        // Use "try" inside "while" that the loop will not be broken.
-        try {
-          // Get but not remove head's element from queue.
-          var timestamp = queue.peek();
-          if (timestamp != null) {
+  private static void runSaver() {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
 
-            // Create connection and check if communication error.
-            Connection connection = null;
-            try {
-              connection = DriverManager.getConnection(DATABASE_URL);
-            } catch (CommunicationsException ex) {
-              LOGGER.warning("DB CONNECTION IS NOT VALID");
-              TimeUnit.SECONDS.sleep(5);
-              continue;
-            } catch (SQLException e) {
-              e.printStackTrace();
-              continue;
-            }
+    while (true) {
+      // Use "try" inside "while" that the loop will not be broken.
+      try {
+        // Get but not remove head's element from queue.
+        var timestamp = queue.peek();
+        if (timestamp != null) {
 
-            // Create and execute statement.
-            var ps = connection.prepareStatement("insert into timestamp (time) values (?)");
-            ps.setTimestamp(1, Timestamp.valueOf(timestamp));
-            ps.execute();
-
-            // Remove head's element from queue (if exception was thrown before this element stays in queue).
-            queue.poll();
-
-            // Release connection.
-            ps.close();
-            connection.close();
+          // Create connection and check if communication error.
+          Connection connection = null;
+          try {
+            connection = DriverManager.getConnection(DATABASE_URL);
+          } catch (CommunicationsException ex) {
+            LOGGER.warning("DB CONNECTION IS NOT VALID");
+            TimeUnit.SECONDS.sleep(5);
+            continue;
+          } catch (SQLException e) {
+            e.printStackTrace();
+            continue;
           }
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    }, "#database");
 
-    timerThread.start();
-    databaseThread.start();
+          // Create and execute statement.
+          var ps = connection.prepareStatement("insert into timestamp (time) values (?)");
+          ps.setTimestamp(1, Timestamp.valueOf(timestamp));
+          ps.execute();
+
+          // Remove head's element from queue (if exception was thrown before this element stays in queue).
+          queue.poll();
+
+          // Release connection.
+          ps.close();
+          connection.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static void runLister() {
+    try (
+        var connection = DriverManager.getConnection(DATABASE_URL);
+        var preparedStatement = connection.prepareStatement("SELECT * from timestamp")
+    ) {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      preparedStatement.setFetchSize(1024);
+      var resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        System.out.println(resultSet.getTimestamp("time"));
+      }
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
