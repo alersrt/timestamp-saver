872409ba9e98200e41b7c15d683bc6a2b13d9935
alersrt <strtmg@yandex.ru>
2018-10-05 08:37:46 +0700

    Optimise connections for using of one per thread

--- src/main/java/org/student/Application.java
+++ src/main/java/org/student/Application.java
@@ -1,9 +1,9 @@
 package org.student;
 
-import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Queue;
@@ -36,17 +36,17 @@ public class Application {
    */
   public static void main(String[] args) {
     if (args.length == 0) {
-      var timerThread = new Thread(Application::runTimer, "#timer");
-      var databaseThread = new Thread(Application::runSaver, "#database");
-      timerThread.start();
-      databaseThread.start();
+      var producer = new Thread(Application::produceTimestamp, "#producer");
+      var consumer = new Thread(Application::consumeTimestamp, "#consumer");
+      producer.start();
+      consumer.start();
     } else if (args.length == 1 && args[0].equals("-p")) {
-      var printerThread = new Thread(Application::runLister, "#list");
-      printerThread.start();
+      var printer = new Thread(Application::printTimestamps, "#printer");
+      printer.start();
     }
   }
 
-  private static void runTimer() {
+  private static void produceTimestamp() {
     while (true) {
       try {
         TimeUnit.SECONDS.sleep(1);
@@ -57,28 +57,29 @@ public class Application {
     }
   }
 
-  private static void runSaver() {
+  private static void consumeTimestamp() {
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
     }
 
+    Connection connection = null;
     while (true) {
       // Use "try" inside "while" that the loop will not be broken.
       try {
         if (!queue.isEmpty()) {
           // Create connection and check if communication error.
-          Connection connection = null;
-          try {
-            connection = DriverManager.getConnection(DATABASE_URL);
-          } catch (CommunicationsException ex) {
-            LOGGER.warning("DB CONNECTION IS NOT VALID");
-            TimeUnit.SECONDS.sleep(5);
-            continue;
-          } catch (SQLException e) {
-            e.printStackTrace();
-            continue;
+          if (connection == null || !connection.isValid(0)) {
+            try {
+              connection = DriverManager.getConnection(DATABASE_URL);
+              // Disable autocommit because we will use transactions.
+              connection.setAutoCommit(false);
+            } catch (SQLException e) {
+              LOGGER.severe(String.format("SQLState: %s | Error code: %d", e.getSQLState(), e.getErrorCode()));
+              TimeUnit.SECONDS.sleep(5);
+              continue;
+            }
           }
 
           // So, we will just reuse existed connection to write
@@ -87,35 +88,37 @@ public class Application {
           while (iterator.hasNext()) {
             // Get but not remove head's element from queue.
             var timestamp = iterator.next();
-            // Savepoint for transaction.
-            var savepoint = connection.setSavepoint();
+            Savepoint savepoint = null;
             // Create and execute statement.
             try (var ps = connection.prepareStatement("insert into timestamp (time) values (?)")) {
+              // Savepoint for transaction.
+              savepoint = connection.setSavepoint();
               ps.setTimestamp(1, Timestamp.valueOf(timestamp));
               ps.execute();
+              connection.commit();
               // Remove head's element from queue (if exception was thrown before this element stays in queue).
               iterator.remove();
             } catch (SQLException e) {
+              LOGGER.severe(String.format("SQLState: %s | Error code: %d", e.getSQLState(), e.getErrorCode()));
               // Why need this try/catch? Connection can be lost when we try
               // to write whole buffer to database. In the next iteration of
               // the main loop we just start from latest place.
               connection.rollback(savepoint);
-              e.printStackTrace();
+              // Release connection.
+              connection.close();
               // Exit from cycle when exception happened.
               break;
             }
           }
-
-          // Release connection.
-          connection.close();
         }
       } catch (Exception e) {
+        System.out.println(e.getClass().getName());
         e.printStackTrace();
       }
     }
   }
 
-  private static void runLister() {
+  private static void printTimestamps() {
     try (
         var connection = DriverManager.getConnection(DATABASE_URL);
         var preparedStatement = connection.prepareStatement("SELECT * from timestamp")
